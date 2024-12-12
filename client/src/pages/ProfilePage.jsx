4682ddc20e7a2fbf7c4d160fec689ee5176d73fb
Ohkrah <dion.cobb.thornhill@gmail.com>
2024-12-11 21:49:56 -0800

    working on getting specific content to display

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import { Box,Card, CardBody, SimpleGrid, 
-    CardFooter, Button, CardHeader, Text, 
+    CardFooter, Button, CardHeader, Text,
     useQuery} from '@chakra-ui/react';
 
 
@@ -8,7 +8,8 @@ import { GET_ALL_EVENTS } from '../utils/queries';
 import  Auth  from '../utils/auth';
 
 const openEventDetails = function (event) {
-    
+    //update after sinngle event page is finished
+   window.location.assign('/singleEvent')
 }
 
 const FollowedEventsPage = () => {
@@ -24,7 +25,7 @@ const FollowedEventsPage = () => {
             console.log('events', data);
             
             try{
-                const eventList = data?.map((item) => ({
+                const joinedEventList = data?.map((item) => ({
                     _id: item.id,
                     homeTeam: item.homeTeam,
                     awayTeam: item.awayTeam,
@@ -52,7 +53,7 @@ const FollowedEventsPage = () => {
                 }];
                 
                 
-                setEvents(eventList)
+                setEvents(joinedEventList)
             }catch (err){
                 console.error(err);
              }
@@ -63,7 +64,9 @@ const FollowedEventsPage = () => {
         
         return (
             <>
-                {user.data.username} 
+                <Text mt={-5} mb={10} fontSize={'30px'}> {user.data.username} </Text>
+                
+                <Text mt={3} mb={2} fontSize={'30px'}>Hosted Events</Text>
                 <Box
                 borderWidth="1px"
                 borderRadius="md"
@@ -88,7 +91,39 @@ const FollowedEventsPage = () => {
                                     </Text>
                                 </CardBody>
                                 <CardFooter justifyContent='flex-end'>
-                                    <Button className="text-black" onClick={() => openEventDetails(item.id)}>View Event</Button>
+                                    <Button mr={6} className="text-black" onClick={() => openEventDetails(item.id)}>View Event</Button>
+                                </CardFooter>
+                            </Card>
+
+                        ) ) }
+                    </SimpleGrid>
+                </Box>
+                <Text mt={3} mb={2} fontSize={'30px'}>Joined Events</Text>
+                <Box
+                borderWidth="1px"
+                borderRadius="md"
+                maxH="350px"
+                overflowY="auto"
+                boxShadow="lg"
+                mt={6}>
+                    
+                    <SimpleGrid
+                    columns={[1,2,3]}
+                    spacing={12}
+                    mt={6} 
+                    mb={5}
+                    ml={2}
+                    mr={2}>
+                        {events.map((item) => (
+                            <Card width='100%' maxWidth='320px' key={item.id} mx='auto'>
+                                <CardBody>
+                                    <CardHeader> {item.homeTeam} vs {item.awayTeam} </CardHeader>
+                                    <Text>
+                                        {item.description}
+                                    </Text>
+                                </CardBody>
+                                <CardFooter justifyContent='flex-end'>
+                                    <Button mr={6} className="text-black" onClick={() => openEventDetails(item.id)}>View Event</Button>
                                 </CardFooter>
                             </Card>
 
